bde0cc1e539892d81802563f19827e331056ea61
pieyx <ijujkh@gmail.com>
2023-10-05 11:55:28 +0300

    Initial Commit

---
+++ Water/WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterApp.swift
+//  Water
+//
+//  Created by Jood Khoja on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
